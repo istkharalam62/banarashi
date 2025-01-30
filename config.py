394ebc126e3686d1219021f9e1ef78f35338f09c
Shivang Mishra <githubshiv@gmail.com>
2025-01-30 17:59:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY","HK543fklqxgt66hvxf")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AbhiModszYT/AnieXEricaMusic",
+    "hhttps://github.com/Shivmis/Artik",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
