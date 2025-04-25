88d2b7cb63ef9e9580c1d76495cc9e70bc66fdaf
BADNAM XD <yadavxd746@gmail.com>
2025-04-25 15:37:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TEAM_BADNAM_BOTS")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TEAM_BADNAM_BOTS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Dil_ki_baate_shayari")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Dil_ki_baate_shayari")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
